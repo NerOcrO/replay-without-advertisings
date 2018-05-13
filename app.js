28964bc05843178617808fd5d11fa8964e44a35b
NerOcrO <nerocro@gmail.com>
2018-05-13 14:53:02 +0200

    favicon added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const express = require('express')
+const favicon = require('serve-favicon')
 const replay_dir = require(`${__dirname}/controller/replay`)
 
 const app = express()
@@ -14,6 +15,9 @@ app.set('view engine', 'ejs')
 app.set('views', `${__dirname}/view`)
 // I don't want to see x-powered-by...
 app.set('x-powered-by', false)
+
+// Favicon.
+app.use(favicon(`${__dirname}/public/favicon.ico`))
 
 // Routing.
 app.use('/', replay_dir)
